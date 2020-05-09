622f05f2f8240e1044d1bd01efd1612bb8ec26a4
lily39 <61435797+lily39@users.noreply.github.com>
2020-05-09 09:21:42 -0500

    Add files via upload

---
+++ miscript.js
@@ -0,0 +1,12 @@
+var nombre= 'Liliana';
+var apellido= 'Marin';
+
+document.write('variable nombre: ' +nombre);
+document.write('variable apellido:' +apellido);
+/*
+Este esun comentario 
+de varias lineas
+y mas
+*/
+
+//Este es un comentario de una sola linea.
